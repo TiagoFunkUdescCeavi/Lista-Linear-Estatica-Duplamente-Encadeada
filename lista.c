79ad3dd02e6198930a5a3012029fda8948c20ba3
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-05-01 17:03:35 -0300

    fix inserir,remover and created buscar

--- lista.c
+++ lista.c
@@ -9,6 +9,10 @@
 #define EMPTY -1
 
 lLista criarLista(int tamanho, int tamanho_elemento) {
+  if (tamanho <= 0) {
+    return NULL;
+  }
+
   lLista ll = malloc(sizeof(Lista));
   ll->tamanho_total_lista = tamanho;
   ll->tamanho_elemento = tamanho_elemento;
@@ -26,12 +30,19 @@ lLista criarLista(int tamanho, int tamanho_elemento) {
   return ll;
 }
 
-int destruirLista(lLista l) {
-  free(l->posicoes_ocupadas);
+int destruirLista(Lista* l) {
+  if (l == NULL || !l || l->tamanho_total_lista <= 0) {
+    return FALSE;
+  }
+
   for (int i = 0; i < l->tamanho_total_lista; i++) {
-    free(l->elementos[i]);
+    if (l->posicoes_ocupadas[i] == TRUE) {
+      free(l->elementos[i]->dado);
+      free(l->elementos[i]);
+    }
   }
   free(l->elementos);
+  free(l->posicoes_ocupadas);
   free(l);
 
   return TRUE;
@@ -40,8 +51,11 @@ int destruirLista(lLista l) {
 int reiniciarLista(lLista l) {
   l->tamanho_atual = 0;
   for (int i = 0; i < l->tamanho_total_lista; i++) {
+    if (l->posicoes_ocupadas[i] == TRUE) {
+      free(l->elementos[i]->dado);
+      free(l->elementos[i]);
+    }
     l->posicoes_ocupadas[i] = FALSE;
-    free(l->elementos[i]);
   }
   l->posicao_primeiro_elemento = EMPTY;
   l->posicao_ultimo_elemento = EMPTY;
@@ -91,17 +105,20 @@ int inserirInicioNaoVazio(lLista l, void *elemento) {
 
   l->elementos[l->posicao_primeiro_elemento]->posicao_anterior = pos;
 
-  l->elementos[pos]->posicao_anterior = EMPTY;
+  l->elementos[pos]->posicao_anterior = l->posicao_ultimo_elemento;
   l->elementos[pos]->posicao_proximo = l->posicao_primeiro_elemento;
+
+  if (l->posicoes_ocupadas[l->posicao_ultimo_elemento] == TRUE) {
+    l->elementos[l->posicao_ultimo_elemento]->posicao_proximo = pos;
+  }
+
   l->posicao_primeiro_elemento = pos;
 
   return TRUE;
 }
 
 int inserirInicio(lLista l, void *elemento) {
-  printf("\n INSERIR INICIO ");
   if (l->posicao_primeiro_elemento == EMPTY) {
-    printf("\n INSERIR INICIO VAZIO");
     return inserirInicioVazio(l, elemento);
   }
   return inserirInicioNaoVazio(l, elemento);
@@ -124,9 +141,12 @@ int inserirFimNaoVazio(lLista l, void *elemento) {
   l->elementos[l->posicao_ultimo_elemento]->posicao_proximo = pos;
 
   l->elementos[pos]->posicao_anterior = l->posicao_ultimo_elemento;
-  l->elementos[pos]->posicao_proximo = EMPTY;
+  l->elementos[pos]->posicao_proximo = l->posicao_primeiro_elemento;
   l->posicao_ultimo_elemento = pos;
 
+  if (l->posicoes_ocupadas[l->posicao_primeiro_elemento] == TRUE) {
+    l->elementos[l->posicao_primeiro_elemento]->posicao_anterior = pos;
+  }
   return TRUE;
 }
 
@@ -139,7 +159,7 @@ int inserirFim(lLista l, void *elemento) {
 
 int inserirNaPosicao(lLista l, void *elemento, int posicao) {
   int pos = procurarVazio(l);
-  if (pos == EMPTY) return FALSE;
+  if (pos == EMPTY) return FALSE;  // verifica se tem lugar para adicionar
 
   escrever(l, elemento, pos);
 
@@ -169,105 +189,150 @@ int inserirPos(lLista l, void *elemento, int posicao) {
   return inserirNaPosicao(l, elemento, posicao);
 }
 
-int max(int v1, int v2) {
-  if (v1 > v2) {
-    return v1;
-  }
-  return v2;
-}
-
 // removerInicio retorna Null caso a lista esteja vazia ou retorna o elemento removido
 void *removerInicio(lLista l) {
-  if (l->tamanho_atual <= 0) { // lista esta vazia
-    return NULL;
-  }
-
-  Elemento *el = l->elementos[l->posicao_primeiro_elemento]; // elemento primeira posicao
-  int new_posicao_primeiro_elemento = el->posicao_proximo; // armazena a posicao do proximo
-  l->posicoes_ocupadas[l->posicao_primeiro_elemento] = FALSE;
-
-  // gera o resultado
-  void *dado = malloc(l->tamanho_elemento);
-  memcpy(dado, el->dado, l->tamanho_elemento);
-
-  // limpa o elemento na primeira posicao
-  free(l->elementos[l->posicao_primeiro_elemento]->dado);
-  free(l->elementos[l->posicao_primeiro_elemento]);
-
-  // atualiza o local da primeira posicao
-  l->posicao_primeiro_elemento = new_posicao_primeiro_elemento;
-  l->tamanho_atual--;
-
-  // caso a lista fique vazia é só retornar o valor
-  if (l->posicao_primeiro_elemento == EMPTY) {
-    return dado;
-  }
-
-  // caso a lista tenha mais valores, deve zerar a posicao anterior do primeiro elemento
-  l->elementos[l->posicao_primeiro_elemento]->posicao_anterior = EMPTY;
-
-  return dado;
+  return removerPos(l, l->posicao_primeiro_elemento);
 }
 
-void *removerPos(lLista l, int pos) {
-  if (l->tamanho_atual <= 0) { // lista esta vazia
+void removerPrimeiroElem(lLista l, int pos) {
+  if (l->posicoes_ocupadas[pos] == TRUE) {
+    l->elementos[pos]->posicao_anterior = l->posicao_ultimo_elemento;
+  }
+
+  if (l->posicao_ultimo_elemento != EMPTY && l->posicoes_ocupadas[l->posicao_ultimo_elemento] == TRUE) {
+    l->elementos[l->posicao_ultimo_elemento]->posicao_proximo = pos;
+  }
+
+  l->posicao_primeiro_elemento = pos;
+}
+
+void removerUltimoElem(lLista l, int pos) {
+  if (l->posicoes_ocupadas[pos] == TRUE) {
+    l->elementos[pos]->posicao_proximo = l->posicao_primeiro_elemento;
+  }
+
+  if (l->posicao_primeiro_elemento != EMPTY && l->posicoes_ocupadas[l->posicao_primeiro_elemento] == TRUE) {
+    l->elementos[l->posicao_primeiro_elemento]->posicao_anterior = pos;
+  }
+
+  l->posicao_ultimo_elemento = pos;
+}
+
+void *removerPos(lLista l, int posicao) {
+  if (l->tamanho_atual <= 0) {  // lista esta vazia
     return NULL;
   }
 
-  if (l->posicoes_ocupadas[l->posicao_primeiro_elemento] == FALSE) {
+  if (posicao >= l->tamanho_total_lista) {  // posicao to remove maior que tamanho total lista
+    return NULL;
+  }
+
+  if (l->posicoes_ocupadas[posicao] == FALSE) {
     // posicao ja esta desocupada
     return NULL;
   }
 
-  Elemento *el = l->elementos[pos]; // elemento na posicao
-  int new_proxima_posicao = el->posicao_proximo; // armazena a posicao do proximo
-  l->posicoes_ocupadas[l->posicao_primeiro_elemento] = FALSE;
+  Elemento *el = l->elementos[posicao];     // elemento na posicao
+  int pos_proximo = el->posicao_proximo;    // armazena a posicao do proximo
+  int pos_anterior = el->posicao_anterior;  // armazena a posicao do anterior
 
-  // gera o resultado
+  // desocupa posicao
+  l->posicoes_ocupadas[posicao] = FALSE;
+
+  // copia o dado retorno
   void *dado = malloc(l->tamanho_elemento);
   memcpy(dado, el->dado, l->tamanho_elemento);
 
-  // limpa o elemento na primeira posicao
-  free(l->elementos[l->posicao_primeiro_elemento]->dado);
-  free(l->elementos[l->posicao_primeiro_elemento]);
+  // limpa o elemento na posicao
+  free(l->elementos[posicao]->dado);
+  free(l->elementos[posicao]);
 
-  // atualiza o local da primeira posicao
-  l->posicao_primeiro_elemento = new_proxima_posicao;
   l->tamanho_atual--;
 
   // caso a lista fique vazia é só retornar o valor
-  if (l->posicao_primeiro_elemento == EMPTY) {
+  if (l->tamanho_atual == 0) {
+    printf("\nREMOVE POS - lista vazia\n");
+    l->posicao_primeiro_elemento = EMPTY;
+    l->posicao_ultimo_elemento = EMPTY;
+    return dado;
+  }
+
+  // esta removendo na primeira posicao
+  if (l->posicao_primeiro_elemento == posicao) {
+    printf("\n REMOVE PRIMEIRA POSICAO %d\n", posicao);
+    removerPrimeiroElem(l, pos_proximo);
+    return dado;
+  }
+
+  // esta removendo na ultima posicao
+  if (l->posicao_ultimo_elemento == posicao) {
+    printf("\n REMOVE ULTIMA POSICAO %d\n", posicao);
+    removerUltimoElem(l, pos_anterior);
     return dado;
   }
 
   // caso a lista tenha mais valores, deve zerar a posicao anterior do primeiro elemento
-  l->elementos[l->posicao_primeiro_elemento]->posicao_anterior = EMPTY;
+  if (l->posicoes_ocupadas[pos_proximo] == TRUE) {
+    l->elementos[pos_proximo]->posicao_anterior = pos_anterior;
+  }
+  if (l->posicoes_ocupadas[pos_anterior] == TRUE) {
+    l->elementos[pos_anterior]->posicao_proximo = pos_proximo;
+  }
 
   return dado;
 }
 
 void *removerFim(lLista l) {
-  if (l->tamanho_atual <= 0) { // lista esta vazia
+  return removerPos(l, l->posicao_ultimo_elemento);
+}
+
+void *buscarInicio(lLista l) {
+  return buscarPos(l, l->posicao_primeiro_elemento);
+}
+
+void *buscarFim(lLista l) {
+  return buscarPos(l, l->posicao_ultimo_elemento);
+}
+
+void *buscarPos(lLista l, int posicao) {
+  if (l->tamanho_atual <= 0) {  // lista esta vazia
     return NULL;
   }
+
+  if (posicao >= l->tamanho_total_lista) {  // posicao maior que tamanho total lista
+    return NULL;
+  }
+
+  if (l->posicoes_ocupadas[posicao] == FALSE) {
+    // posicao vazia
+    return NULL;
+  }
+
+  // busca o elemento para copiar o dado
+  Elemento *el = l->elementos[posicao];
+  void *dado = malloc(l->tamanho_elemento);
+  memcpy(dado, el->dado, l->tamanho_elemento);
+
+  return dado;
 }
 
 void mostrarListaInt(lLista l) {
   printf("TAMANHO_TOTAL_LISTA: %d\n", l->tamanho_total_lista);
   printf("TAMANHO_ELEMENTO: %d\n", l->tamanho_elemento);
   printf("TAMANHO_ATUAL: %d\n", l->tamanho_atual);
-  if (l->tamanho_atual <= 0) {
-    printf("\n\n");
-    return;
-  }
 
   printf("POSICOES_OCUPADAS: ");
   for (int i = 0; i < l->tamanho_total_lista; i++) {
     printf("%d, ", l->posicoes_ocupadas[i]);
   }
+
   printf("\n");
   printf("POSICAO_PRIMEIRO_ELEMENTO: %d\n", l->posicao_primeiro_elemento);
   printf("POSICAO_ULTIMO_ELEMENTO: %d\n", l->posicao_ultimo_elemento);
+  if (l->tamanho_atual <= 0) {
+    printf("\n\n");
+    return;
+  }
   printf("ELEMENTOS NA LISTA:\n");
   for (int i = 0; i < l->tamanho_total_lista; i++) {
     mostrarElemento(l->elementos[i], i, l->posicoes_ocupadas[i]);
@@ -275,9 +340,12 @@ void mostrarListaInt(lLista l) {
   printf("POSICOES EM ORDEM: ");
   int a = l->posicao_primeiro_elemento;
   Elemento *el = l->elementos[l->posicao_primeiro_elemento];
-  while (el != 0) {
+  while (el != 0 && el->posicao_proximo != l->posicao_primeiro_elemento) {
     printf("%d, ", a);
-    if (el->posicao_proximo == EMPTY) break;
+    if (el->posicao_proximo == EMPTY || el->posicao_proximo == l->posicao_primeiro_elemento) {
+      break;
+    }
+
     a = el->posicao_proximo;
     el = l->elementos[el->posicao_proximo];
   }
@@ -288,7 +356,7 @@ void mostrarListaInt(lLista l) {
   while (el != 0) {
     d = el->dado;
     printf("%d, ", *d);
-    if (el->posicao_proximo == EMPTY) break;
+    if (el->posicao_proximo == EMPTY || el->posicao_proximo == l->posicao_primeiro_elemento) break;
     el = l->elementos[el->posicao_proximo];
   }
   printf("\n");
@@ -299,7 +367,7 @@ void mostrarListaInt(lLista l) {
   while (el != 0) {
     d = el->dado;
     printf("%d, ", *d);
-    if (el->posicao_anterior == EMPTY) break;
+    if (el->posicao_anterior == EMPTY || el->posicao_anterior == l->posicao_ultimo_elemento) break;
     el = l->elementos[el->posicao_anterior];
   }
   printf("\n\n\n");
@@ -315,4 +383,11 @@ void mostrarElemento(Elemento *el, int posicao, int posicaoOcupada) {
   printf("\tDADO: %d\n", *d);
   printf("\tPOSICAO_ANTERIOR: %d\n", el->posicao_anterior);
   printf("\tPOSICAO_PROXIMO: %d\n", el->posicao_proximo);
+}
+
+void printElementosLista(lLista l) {
+  printf("ELEMENTOS NA LISTA:\n");
+  for (int i = 0; i < l->tamanho_total_lista; i++) {
+    mostrarElemento(l->elementos[i], i, l->posicoes_ocupadas[i]);
+  }
 }
