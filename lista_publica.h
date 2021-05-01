48bca98d8fa41428af83229728c948b232a98948
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-05-01 10:42:06 -0300

    idented file

--- lista_publica.h
+++ lista_publica.h
@@ -4,21 +4,21 @@
 typedef struct Lista *lLista;
 
 lLista criarLista(int tamanho, int tamanho_elemento);
-int destruirLista( lLista l );
-int reiniciarLista( lLista l );
+int destruirLista(lLista l);
+int reiniciarLista(lLista l);
 
-int inserirInicio( lLista l, void * elemento );
-int inserirFim( lLista l, void * elemento );
-int inserirPos( lLista l, void * elemento, int posicao );
+int inserirInicio(lLista l, void *elemento);
+int inserirFim(lLista l, void *elemento);
+int inserirPos(lLista l, void *elemento, int posicao);
 
-void * removerInicio( lLista l );
-void * removerFim( lLista l );
-void * removerPos( lLista l, int posicao );
+void *removerInicio(lLista l);
+void *removerFim(lLista l);
+void *removerPos(lLista l, int posicao);
 
-void * buscarInicio( lLista l );
-void * buscarFim( lLista l );
-void * buscarPos( lLista l, int posicao );
+void *buscarInicio(lLista l);
+void *buscarFim(lLista l);
+void *buscarPos(lLista l, int posicao);
 
-void mostrarLista( lLista l );
+void mostrarListaInt(lLista l);
 
 #endif
